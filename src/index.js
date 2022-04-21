4eca71d3a229012478a3ce829cc2832f0386c893
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-21 21:58:29 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+let a = new Node("A");
+let b = new Node("B");
+let c = new Node("C");
+let d = new Node("D");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+function linkedListValues() {}
